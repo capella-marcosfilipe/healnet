3e1d17ea3ad5f25fb615f5366cc60c8a7e157486
odaniellevi <daniellevi.unicap@gmail.com>
2024-06-11 15:07:44 -0300

    implementação do JavaScript

--- scripts/depoimentos.js
+++ scripts/depoimentos.js
@@ -1,18 +1,21 @@
-const observerOptions = {
-    threshold: 0.1
-};
+document.addEventListener('DOMContentLoaded', function() {
+    const observerOptions = {
+        threshold: 0.1
+    };
 
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-            entry.target.classList.remove('hidden');
-            observer.unobserve(entry.target);
-        }
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                entry.target.classList.remove('hidden');
+            } else {
+                entry.target.classList.remove('visible');
+                entry.target.classList.add('hidden');
+            }
+        });
+    }, observerOptions);
+
+    document.querySelectorAll('.depoimento-box').forEach(box => {
+        observer.observe(box);
     });
-}, observerOptions);
-
-document.querySelectorAll('.depoimento-box').forEach(box => {
-    box.classList.add('hidden');
-    observer.observe(box);
 });
